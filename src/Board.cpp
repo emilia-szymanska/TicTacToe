5894e73a2b22089bb61d4c3a3bbdccd63aef1da5
emilia-szymanska <emiliaszym@gmail.com>
2020-05-08 17:04:06 +0200

    displaying the board in a better way for dimensions >= 10

--- src/Board.cpp
+++ src/Board.cpp
@@ -2,37 +2,48 @@
 
 void Board::displayBoard(Player playerName1, Player playerName2)
 {
+	cout << endl;
+
 	for(unsigned int i = 0; i <= this->returnSize(); i++)
 	{
 		for(unsigned int j = 0; j <= this->returnSize(); j++)
 		{
 			if(i == 0) 							//displaying a row of numbers
 			{
-				if(j == 0) cout << "  " << j+1;
-				else if(j != this->returnSize()) cout << " " << j+1;
+				if(j == 0)
+				{	cout << "   " << j+1;}
+				else {
+					if(j != this->returnSize()) 
+					{
+						if (j<10) cout << "  " << j+1;
+						else cout << " " << j+1;
+					}
+				}	
 			}
 			else 
 			{
 				if(j == 0) 
 				{
-					cout << i << " ";				//displaying the number of the line
+					if( i >= 10) cout << i << " ";
+					else cout << i << "  ";				//displaying the number of the line
 				}
 				else 
 				{
 					if(this->matrixOfFields(i-1,j-1) == playerName1.playerID) 
 					{
-						cout << playerName1.playerSign << " ";
+						cout << playerName1.playerSign << "  ";
 					}
 					else 
 					{
-						if(this->matrixOfFields(i-1,j-1) == playerName2.playerID) cout << playerName2.playerSign << " ";
-						else cout << "_ ";
+						if(this->matrixOfFields(i-1,j-1) == playerName2.playerID) cout << playerName2.playerSign << "  ";
+						else cout << "_  ";
 					}	
 				}
 			}
 		}
 		cout << endl;
 	}
+	cout << endl;
 }
 
 
